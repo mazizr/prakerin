397132fcddad4aeafe7fe33765f6081f6747625b
mazizr <saitamasensei710@gmail.com>
2019-07-02 14:33:25 +0700

    nambah auth dan database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Aziz Riza
                 </div>
 
                 <div class="links">
