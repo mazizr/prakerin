4ee2b7bbd03b3a4a1a54271c5b6b90482c278cf6
mazizr <saitamasensei710@gmail.com>
2019-07-06 09:23:54 +0700

    pengaturan tampilan

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-    <!-- Title  -->
-    <title>Yuriza - Website Artikel</title>
-
-    <!-- Favicon  -->
-    <link rel="icon" href="{{ asset('assets/frontend/img/core-img/iconnya.ico') }}">
-
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="{{asset('assets/frontend/style.css')}}">
-    @yield('css')
-</head>
-
-<body>
+@extends('layouts.frontend')
     <!-- Preloader Start -->
     <div id="preloader">
         <div class="preload-content">
@@ -28,63 +7,7 @@
     </div>
     <!-- Preloader End -->
 
-    {{--  @section('content')  --}}
-
-    <!-- ***** Header Area Start ***** -->
-<header class="header-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="navbar navbar-expand-lg">
-                    <!-- Logo -->
-                    <a class="navbar-brand" href="index.html"><b class="napbarteks">YURIZA</b></a>
-                    <!-- Navbar Toggler -->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#worldNav" aria-controls="worldNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                    <!-- Navbar -->
-                    <div class="collapse navbar-collapse" id="worldNav">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Horor</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Misteri</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pengetahuan</a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="index.html">Sejarah</a>
-                                    <a class="dropdown-item" href="catagory.html">kosong</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Gaya Hidup</a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="index.html">travelling</a>
-                                    <a class="dropdown-item" href="catagory.html">ehe</a>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{url('about')}}">About</a>
-                            </li>
-                        </ul>
-                        <!-- Search Form  -->
-                        <div id="search-wrapper">
-                            <form action="#">
-                                <input type="text" id="search" placeholder="Search something...">
-                                <div id="close-icon"></div>
-                                <input class="d-none" type="submit" value="">
-                            </form>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </div>
-</header>
-<!-- ***** Header Area End ***** -->
+    @section('content')
         
     
 
@@ -2803,60 +2726,6 @@
         </div>
     </div>
 
-    <!-- ***** Footer Area Start ***** -->
-<footer class="footer-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-4">
-                <div class="footer-single-widget">
-                    <a href="#"><img src="{{ ('assets/frontend/img/core-img/logo.png')}}" alt=""></a>
-                    <div class="copywrite-text mt-30">
-                        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4">
-                <div class="footer-single-widget">
-                    <ul class="footer-menu d-flex justify-content-between">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Fashion</a></li>
-                        <li><a href="#">Lifestyle</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Gadgets</a></li>
-                        <li><a href="#">Video</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-12 col-md-4">
-                <div class="footer-single-widget">
-                    <h5>Subscribe</h5>
-                    <form action="#" method="post">
-                        <input type="email" name="email" id="email" placeholder="Enter your mail">
-                        <button type="button"><i class="fa fa-arrow-right"></i></button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- ***** Footer Area End ***** -->
-
-<!-- jQuery (Necessary for All JavaScript Plugins) -->
-<script src="{{ ('assets/frontend/js/jquery/jquery-2.2.4.min.js') }}"></script>
-<!-- Popper js -->
-<script src="{{ ('assets/frontend/js/popper.min.js') }}"></script>
-<!-- Bootstrap js -->
-<script src="{{ ('assets/frontend/js/bootstrap.min.js') }}"></script>
-<!-- Plugins js -->
-<script src="{{ ('assets/frontend/js/plugins.js') }}"></script>
-<!-- Active js -->
-<script src="{{ ('assets/frontend/js/active.js') }}"></script>
-@yield('js')
-
-</body>
-
-</html>
+    @endsection
 
   
